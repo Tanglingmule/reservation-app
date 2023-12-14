5a283fc79d66f6aabb14cc6c1737edcd60cd8789
Tanglingmule <zain.khansia3@gmail.com>
2023-12-14 16:33:48 +0000

    development

--- reservation-react-app/src/mainPage.jsx
+++ reservation-react-app/src/mainPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import CompanyHeader from './components/company-header';
+import InputButton from './components/button';
+import './components/mainPage.css'
+import { useNavigate } from 'react-router-dom'; 
 
 function MainPage() {
+
+    let navigate = useNavigate();
+    const moveMakeReservation = () => {
+        navigate('/makeReservation')
+    }
+
+    const moveDeleteReservation = () => {
+        navigate('/deleteReservation')
+    }
+
+    const moveRewards = () => {
+        navigate('rewards')
+    }
+
     return(
-        <CompanyHeader name = 'Tesco'/>
+        <div>
+            <div id = 'companyHead'>
+                <CompanyHeader name = 'Tesco'/>
+            </div>
+            <div id = 'buttons'>
+                <InputButton text={'Make Reservation'} functionToDo={moveMakeReservation}/>
+                <InputButton text={'Delete Reservation'} functionToDo={moveDeleteReservation}/>
+                <InputButton text={'Reward Points'} functionToDo={moveRewards}/>
+            </div>
+        </div>
     )
 }
 
